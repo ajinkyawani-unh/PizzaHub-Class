2821b32a418cfa4051910e94d43f4a41e2aa3de5
chefeleUNH <chefele@newhaven.edu>
2020-04-16 19:41:42 -0400

    added photo property to menu item

--- PizzaHub/Model/MenuItem.swift
+++ PizzaHub/Model/MenuItem.swift
@@ -12,17 +12,20 @@ class MenuItem: FirebaseCodable {
     var id: String
     @Published var name: String
     @Published var price: String
+    @Published var photo: String
     
     var data: [String: Any] {
         return [
             "name": name,
             "price": price,
+            "photo": photo
         ]
     }
     
     required init?(id: String, data: [String : Any]) {
         guard let name = data["name"] as? String,
-            let price = data["price"] as? String
+            let price = data["price"] as? String,
+            let photo = data["photo"] as? String
             else {
                 return nil
         }
@@ -30,5 +33,12 @@ class MenuItem: FirebaseCodable {
         self.id = id
         self.name = name
         self.price = price
+        self.photo = photo
     }
+    
+    #if DEBUG
+    static let example = MenuItem(id: "1", data: ["name": "Cheese Pizza",
+                                                  "price": "17.99",
+                                                  "photo": "1"])!
+    #endif
 }
