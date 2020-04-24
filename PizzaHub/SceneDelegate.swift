f9306a5ee5214938d8d35497e753ae6f420820d2
chefeleUNH <chefele@newhaven.edu>
2020-04-23 21:09:03 -0400

    added simulated push notification on pizzeria order ready status

--- PizzaHub/SceneDelegate.swift
+++ PizzaHub/SceneDelegate.swift
@@ -18,7 +18,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
+        
+        // request notifications from user
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+            if success {
+                print("Notifications enabled by user")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
         // Create the SwiftUI view that provides the window contents.
         let appTabView = AppTabView().environmentObject(cart)
 
--- PizzaHub/View/CheckoutView.swift
+++ PizzaHub/View/CheckoutView.swift
@@ -29,7 +29,27 @@ struct CheckoutView: View {
     }
     
     func confirmOrder() {
+        // toggle the payment alert
         self.showingPaymentAlert.toggle()
+        
+        guard let pizzeria = self.cart.pizzeria else {
+            return
+        }
+        
+        // show pizza ready notification to the user
+        let content = UNMutableNotificationContent()
+        content.title = "Pizza order ready!"
+        content.subtitle = "Your order from \(pizzeria.name) is ready for pickup."
+        content.sound = UNNotificationSound.default
+        
+        // show this notification five seconds from now
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // choose a random identifier
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // add our notification request
+        UNUserNotificationCenter.current().add(request)
     }
 }
 
